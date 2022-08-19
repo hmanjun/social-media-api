72ebae9d4fba8ca20e9dddab9452b75ca06ad303
hmanjun <harshith.hm03@gmail.com>
2022-08-18 18:07:23 -0700

    Add function to add friend to a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,4 +52,18 @@ async function deleteUser(req,res) {
     }
 }
 
-module.exports = {getUser, getSingleUser, createUser, updateUser, deleteUser}
+async function addFriend(req,res) {
+    try {
+        const userData = await User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$addToSet: {friends: req.params.friendId}},
+            { runValidators: true, new: true }
+        )
+        ! userData ? res.status(404).json({message: `No user found with that id`})
+        : res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+module.exports = {getUser, getSingleUser, createUser, updateUser, deleteUser, addFriend}
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 
 //install controllers
-const {getUser,getSingleUser,createUser,updateUser,deleteUser} = require('../../controllers/userController')
+const {getUser,getSingleUser,createUser,updateUser,deleteUser,addFriend} = require('../../controllers/userController')
 
 //add controllers to get and post
 router.route('/').get(getUser).post(createUser)
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser)
 
-router.route('/:userId/friends/friendId').post().delete()
+router.route('/:userId/friends/:friendId').post(addFriend).delete()
 
 module.exports = router
